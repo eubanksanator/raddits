17bb970c08d6cc8a6b59c6b0d74e4c09685885b4
Travis Eubanks <TRAVIS@traviss-air.attlocal.net>
2015-03-08 17:30:51 -0500

    add username to Users

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,6 +2,8 @@ class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:index, :show]
 
+
+
   # GET /links
   # GET /links.json
   def index
